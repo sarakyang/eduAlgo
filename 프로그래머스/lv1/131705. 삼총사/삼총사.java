0d1cb8d4567d88c5035f55a503c739cab2248418
sarakyang <139844539+sarakyang@users.noreply.github.com>
2023-08-23 16:23:49 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,23 +1,6 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        
-        int A = 0;
-        int B = 0;
-        int c = 0;
-
-        for (int i = 0 ; i < number.length ; i++) {
-            for (int j = i+1 ; j < number.length  ; j++) {
-                for(int k = j+1 ; k < number.length  ; k ++) {
-                    A = number[i];
-                    B = number[j];
-                    c= number[k];
-                    if(A+B+c == 0) {answer++;}
-
-                }
-            }
-        }
-        
         return answer;
     }
 }
