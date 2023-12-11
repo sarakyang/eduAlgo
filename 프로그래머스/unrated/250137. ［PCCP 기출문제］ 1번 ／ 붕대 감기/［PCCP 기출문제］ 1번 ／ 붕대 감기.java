7e289c59768536c6eac83ec5bb1cdc1f223ba8e9
sarakyang <139844539+sarakyang@users.noreply.github.com>
2023-12-11 11:19:55 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.54 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = 0;
+
+        int health_now = health;
+        int success = 0;
+        int time = attacks[attacks.length-1][0];
+        int t = 0;
+
+        Map<Integer, Integer> damage = new HashMap<>();
+        for (int[] attack : attacks) {
+            damage.put(attack[0],attack[1]);
+        }
+
+        while (t <= time) {
+            if (damage.containsKey(t)) {
+                health_now -= damage.get(t);
+                success = 0;
+                t++;
+                if (health_now <= 0) {
+                    health_now = -1;
+                    break;
+                }
+                continue;
+            }
+
+            health_now+= bandage[1];
+            success++;
+            if (success == bandage[0]) {
+                health_now += bandage[2];
+                success = 0;
+            }
+            if (health_now >= health) health_now = health;
+
+            t++;
+        }
+        if (health_now <= 0) health_now = -1;
+
+        return health_now;
+    }
+}
