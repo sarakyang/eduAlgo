9fa2690f806c08b2357105c39fcba2dc03acd110
sarakyang <139844539+sarakyang@users.noreply.github.com>
2023-12-15 16:21:39 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.66 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+    public String solution(String new_id) {
+        
+        //1단계
+        new_id = new_id.toLowerCase();
+        //2단계
+        Pattern pattern = Pattern.compile("[^a-z0-9-_.]");
+        Matcher matcher = pattern.matcher(new_id);
+        new_id = matcher.replaceAll("");
+        //3단계
+        while (new_id.contains("..")) {
+            new_id = new_id.replace("..", ".");
+        }
+        //4단계
+        if(new_id.charAt(0) == '.') new_id = new_id.replaceFirst(".","");
+        if(new_id.length() >= 2 && new_id.charAt(new_id.length()-1) == '.') new_id = new_id.substring(0,new_id.length()-1);
+        //5단계
+        if(new_id.isEmpty()) new_id += "a";
+        //6단계
+         if(new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+            while (new_id.charAt( new_id.length()-1) == '.') new_id = new_id.substring(0,new_id.length()-1);
+        }
+        //7단계
+        if(new_id.length() <= 2) while (new_id.length() < 3) new_id = new_id + new_id.charAt(new_id.length()-1);
+
+        return new_id;
+    }
+}
